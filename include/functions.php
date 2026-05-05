3c94ef88de69260e8ed894d969812dbd8d953e4b
saad eddine laakik <saadlk1997@gmail.com>
2026-05-05 01:20:54 +0100

    Fix login query with prepared statement

--- include/functions.php
+++ include/functions.php
@@ -7,13 +7,25 @@ function redirect($location){
 }
 function logintempt($username,$password){
   global $link ;
-  $req = "SELECT * FROM information where mail='$username' AND password='$password'";
-  $exec = mysqli_query($link,$req);
-  if ($admin = mysqli_fetch_assoc($exec)) {
-    return $admin;
-  }else {
+  $username = trim($username);
+  $password = trim($password);
+
+  $stmt = mysqli_prepare($link, "SELECT Id_user, nom, prenom, adresse, ville, code, tel, mail, password FROM information WHERE mail = ? AND password = ? LIMIT 1");
+  if (!$stmt) {
     return null;
   }
+
+  mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+
+  if ($admin = mysqli_fetch_assoc($result)) {
+    mysqli_stmt_close($stmt);
+    return $admin;
+  }
+
+  mysqli_stmt_close($stmt);
+  return null;
 }
 function login(){
   if (isset($_SESSION["user_Id"])) {
